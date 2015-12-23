962515405ead44e79a29a43a35889884de6cc468
David Conner <dconner.pro@gmail.com>
2015-12-23 02:23:58 -0500

    corrected csv funciton

--- img_download.py
+++ img_download.py
@@ -4,10 +4,11 @@ import csv
 
 def read_from_csv(filename):
     rows = []
-    csvrdr = csv.reader(filename)
-    for row in csvrdr:
-        rows.append(row)
-    rows
+    with open(filename, 'r') as f:
+        csvrdr = csv.reader(f)
+        for row in csvrdr:
+            rows.append(row)
+    return rows
 
 # load data from csv
 CSV_DX = './img-dx.csv'
@@ -16,4 +17,4 @@ CSV_TS = './img-ts.csv'
 img_dx = read_from_csv(CSV_DX)
 img_ts = read_from_csv(CSV_TS)
 
-print(img_dx)
+print(img_dx[0])
