1567f5db4e5f81c31f2060bd17ceaebd56abc21a
David Conner <dconner.pro@gmail.com>
2015-12-29 14:22:05 -0500

    added notes about the color deconv algorithm

--- src/color_deconv.py
+++ src/color_deconv.py
@@ -15,6 +15,60 @@
 # byte [] gLUT = new byte[256];
 # byte [] bLUT = new byte[256];
 
+# ==== [Algorithm in Pseudocode] ==================================
+
+# Note: may need to identify the background color first
+
+# set LEN and normalize COS
+# zero(COS[i])
+# MOD[i] = assign predefined constants
+# LEN[i] = normalize MOD[i]
+# COS[i] = MOD[i]/len[i], unless LEN[i] == zero
+
+# if COS[1] is [0,0,0] (unspecified)
+#   zero COS[1]
+
+# if COS[2] is [0,0,0] (unspecified)
+#   foreach a in x,y,z in COS
+#     if a[0]^2 + a[0]^2 > 1
+#       # this means color has a negative R component
+#       a[2] = 0
+#     else
+#       a[2] = sqrt(1.0 - (a[0]^2 + a[1]^2))
+
+# then, normalize COS[2]
+# leng = dist(COS[2])
+# COS[2] = COS[2] / leng
+
+# remove zeros (realllyy?)
+# COS[i] = 0.001 if COS[i] == 0
+
+# invert matrix
+#  .... wow this article is so much simpler than the java code (MATRICES!!)
+# - http://web.hku.hk/~ccsigma/color-deconv/color-deconv.html
+# M = COS
+# D = inv(M)
+
+
+
+# apply matrix deconvolution
+
+
+# ===============================================================
+
+MOD_HNE1 = {
+    'x': [],
+    'y': [],
+    'z': []
+}
+
+MOD_HNE2 = {
+    'x': [],
+    'y': [],
+    'z': []
+}
+
+
 # H&E1
 # GL Haem matrix
 MODx[0]= 0.644211; #0.650;
