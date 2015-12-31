87d9702e98c53642c3b711a9b57d8ffcec1fab62
David Conner <dconner.pro@gmail.com>
2015-12-31 04:24:40 -0500

    trying to figure out how to import python files into a notebook

---
+++ apoptosiscv/pcf_data_set.py
@@ -0,0 +1,11 @@
+import os.path
+import csv
+
+def read_training_samples(csvname):
+    samples = [] # { 'id', 'fga' }
+    with open(csvname, 'r') as f:
+        csvrdr = csv.reader(f)
+        for row in csvrdr:
+            samples.append({'id': row[0], 'fga': row[1]})
+    return rows
+
