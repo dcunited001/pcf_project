54a5b55b95e7d903812ca044c44611fa0fdb646d
David Conner <dconner.pro@gmail.com>
2015-12-31 05:48:48 -0500

    added code to lib to read data from csv and set configs for really dumbass simple shit

--- apoptosiscv/pcf_data_set.py
+++ apoptosiscv/pcf_data_set.py
@@ -1,5 +1,21 @@
+import apoptosiscv
 import os.path
 import csv
+
+def config_paths(path):
+    return {'home': path,
+            'data': os.path.join(path, 'data'),
+            'tf': os.path.join(path, 'tfdata'),
+            'dx': os.path.join(path, 'img/dx'),
+            'ts': os.path.join(path, 'img/ts')}
+
+# this_module.HOMEDIR = os.path.dirname(__file__)
+
+# HOMEDIR = os.path.dirname(__file__)
+# DATADIR =  os.path.join(HOMEDIR, 'data')
+# TFDATA = os.path.join(HOMEDIR, 'tfdata')
+# DXPATH = os.path.join('img/dx')
+# TSPATH = os.path.join('img/ts')
 
 def read_training_samples(csvname):
     samples = [] # { 'id', 'fga' }
@@ -7,6 +23,6 @@ def read_training_samples(csvname):
         csvrdr = csv.reader(f)
         for row in csvrdr:
             samples.append({'id': row[0], 'fga': row[1]})
-    return rows
+    return samples
 
 
